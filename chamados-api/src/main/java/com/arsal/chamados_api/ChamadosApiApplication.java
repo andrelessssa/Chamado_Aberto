0d142f5ab93a97b7c3cd939b051db136788939d9
andre.lessa <andrelessa013@gmail.com>
2026-07-01 13:23:13 -0300

    :tada:	init Projeto iniciado
    :tada:	init Projeto iniciado

---
+++ chamados-api/src/main/java/com/arsal/chamados_api/ChamadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.arsal.chamados_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosApiApplication.class, args);
+	}
+
+}
---
+++ chamados-api/src/test/java/com/arsal/chamados_api/ChamadosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsal.chamados_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
